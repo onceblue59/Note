2f6dfe5517cd2842492bd1f650cfcea01c27e23c
onceblue59 <152699143+onceblue59@users.noreply.github.com>
2026-07-14 22:23:23 +0800

    config: update site and base for onceblue59/Note

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://shgzg.github.io/iNote',
-  base: '/iNote',
+  site: 'https://onceblue59.github.io/Note',
+  base: '/Note',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
